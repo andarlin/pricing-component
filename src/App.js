7dde6540116139a054e4a071814ace8ba1a3d713
Andar Lin <andar.lin7@gmail.com>
2020-08-06 12:22:37 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import PricingContainer from "./pricing-component/pricing-component-container";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <PricingContainer />
+    </div>
+  );
+}
